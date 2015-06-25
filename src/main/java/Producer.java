0fa63875d1245304499aec6e971d36580a6c2675
Habib <ismail.muhammad@mendix.com>
2015-06-25 12:57:18 +0200

    First commit

---
+++ src/main/java/Producer.java
@@ -0,0 +1,32 @@
+/**
+ * Created by imu on 6/25/2015.
+ */
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class Producer {
+
+	public static void main (String[] args) throws ExecutionException, InterruptedException {
+
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "128.199.82.219:9092");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+		KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+		String msg = "test message";
+		String topic = "test";
+
+		ProducerRecord<String,String> producerRecord = new ProducerRecord<>(topic, msg);
+
+		producer.send(producerRecord).get();
+		producer.close();
+	}
+}
